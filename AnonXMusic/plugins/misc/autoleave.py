4dad032fb2cb951e345143da81be9c4b4d650006
MR NEON <141004134+mrneon1605@users.noreply.github.com>
2024-05-28 10:22:51 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001896959975
+                                and i.chat.id != -1001896959975
                             ):
                                 if left == 20:
                                     continue
