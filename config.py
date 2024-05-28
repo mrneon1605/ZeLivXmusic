1546afff27050582a03f716550c6cee3275baf53
MR NEON <141004134+mrneon1605@users.noreply.github.com>
2024-05-28 10:11:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 6000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 5292020082))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/oye_mai_teri_aaru")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dosto_ki_dosti_1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THESUKOONLYRIC")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/THESUKOONLYRIC")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
